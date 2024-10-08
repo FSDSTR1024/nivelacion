0e938f0f669a414a52a7660422aa10bf7e0be2b3
DiegoRol69 <drol@ucm.es>
2024-10-08 17:54:03 +0200

    arbol de asteriscos y pagina de toDo hecha bien en js

--- arbol.js
+++ arbol.js
@@ -1,1 +1,21 @@
-console.log("hola")
+
+let height = 10;
+
+let leaf = '*';
+
+for(let i = 0; i  < height; i++){
+
+    let spaces = '';
+
+    for(let k = 0; k < height - i - 1; k++){
+        spaces += ' ';
+    }
+
+    let auxLeaf = spaces;
+
+    for(let j = 0; j < i+1; j++){
+        auxLeaf += leaf + ' ';
+    }
+
+    console.log(auxLeaf);
+}
--- index.js
+++ index.js
@@ -21,11 +21,23 @@ document.getElementById("taskform").addEventListener('submit', function(e){
 // se puede llamar a este metodo sin que esté definido arriba
 function addTask(description, dueDate){
     const newTask = document.createElement("div");
-    const tastText = document.createElement("p");
+    
+    // Texto de la tarea
+    const taskText = document.createElement("p");
+    
+    // Texto dentro de la tarea que contiene el estado de la tarea: toDo, done,etc
+    const taskState = document.createElement("p");
+    const taskStateChild = document.createElement("span");
+    taskStateChild.className = "inProgress";
+    taskStateChild.innerHTML = `IN PROGRESS`
 
-    tastText.innerHTML = `Descripción: ${description} <br> Fecha límite: ${dueDate}`;
+    taskText.innerHTML = `Descripción: ${description} <br>`;
+    taskState.appendChild(taskStateChild);
+    taskState.innerHTML += ` Fecha límite: ${dueDate}`;
 
-    newTask.appendChild(tastText);
+    newTask.className = 'listItems';
+    newTask.appendChild(taskText);
+    newTask.appendChild(taskState);
 
     const buttons = document.createElement('p');
 
@@ -34,6 +46,7 @@ function addTask(description, dueDate){
     editButton.text = 'EDITAR';
     editButton.addEventListener('click', function(){
         // Editar tarea
+        editTask(newTask, description, dueDate);
     })
 
     const endTaskButton = document.createElement('a');
@@ -41,16 +54,22 @@ function addTask(description, dueDate){
     endTaskButton.text = 'MARCAR COMO FINALIZADA';
     endTaskButton.addEventListener('click', function(){
         // Finalizar tarea
+        endTask(taskState);
     })
 
     const deleteButton = document.createElement('a');
     deleteButton.href = '#';
     deleteButton.text = 'ELIMINAR';
+    deleteButton.className = 'eliminar'
     deleteButton.addEventListener('click', function(){
         // Eliminar tarea
+        deleteTask(newTask);
     })
 
     buttons.appendChild(editButton);
+    buttons.appendChild(endTaskButton);
+    buttons.appendChild(deleteButton);
+
     newTask.appendChild(buttons);
 
     document.getElementById("tasks").appendChild(newTask);
@@ -62,4 +81,14 @@ function editTask(newTask, oldDescription, oldDueDate){
     const newDueDate = prompt('Indica nueva fecha. Antigua:', oldDueDate);
 
     newTask.querySelector("p").innerHTML = `Descripción: ${newDescription} <br> Fecha límite: ${newDueDate}`;
+}
+
+function endTask(taskState){
+
+    taskState.querySelector('.inProgress').innerHTML = "DONE";
+    taskState.querySelector('.inProgress').className = "done";
+}
+
+function deleteTask(newTask){
+    document.getElementById("tasks").removeChild(newTask);
 }
