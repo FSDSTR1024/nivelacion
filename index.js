57021bc2e5e0c5e65b8f0535cc35d81cd53b3316
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-13 23:29:35 +0200

    Add the JS code, learnt in class and self done too

---
+++ index.js
@@ -0,0 +1,61 @@
+function addNewTaskToTasksList(newTaskDescription, newTaskDueDate) {
+  /* 1. Create the new Task object */
+  const taskContainer = document.createElement("section");
+  taskContainer.classList.add("task");
+  /* 2. Add the new task description */
+  const taskDescription = document.createElement('p');
+  taskDescription.innerHTML = newTaskDescription;
+  taskContainer.appendChild(taskDescription);
+  /* 3. Add the new task due date */
+  const taskDueDateContainer = document.createElement('p');
+  taskDueDateContainer.classList.add("date");
+  /* 3.1. Status */
+  const taskDueDateStatus = document.createElement("span");
+  taskDueDateStatus.classList.add("upper");
+  taskDueDateStatus.classList.add("todo");
+  taskDueDateStatus.innerHTML = "To Do"
+  taskDueDateContainer.appendChild(taskDueDateStatus);
+  /* 3.2. Due date text */
+  taskDueDateContainer.innerHTML += ` Due date: ${newTaskDueDate}`;
+  taskContainer.appendChild(taskDueDateContainer);
+  /* 4. Add the new task buttons */
+  const taskButtonsContainer = document.createElement('p');
+  /* 4.1. Edit */
+  const editButton = document.createElement('a');
+  editButton.classList.add("edit");
+  editButton.href = '#';
+  editButton.innerHTML = "Edit";
+  taskButtonsContainer.appendChild(editButton);
+  /* 4.2. Mark as Finished */
+  const markAsFinishedButton = document.createElement('a');
+  markAsFinishedButton.classList.add("markAsFinished");
+  markAsFinishedButton.href = '#';
+  markAsFinishedButton.innerHTML = "Mark as Finished";
+  taskButtonsContainer.appendChild(markAsFinishedButton);
+  /* 4.3. Delete */
+  const deleteButton = document.createElement('a');
+  deleteButton.classList.add("delete");
+  deleteButton.href = '#';
+  deleteButton.innerHTML = "Delete";
+  taskButtonsContainer.appendChild(deleteButton);
+  taskContainer.appendChild(taskButtonsContainer);
+  /* 5. Add the new task to the tasks list */
+  document.getElementById("tasksSection").appendChild(taskContainer);
+}
+
+/* Listener function when 'Submit' forms button is clicked, to create a new task */
+document.getElementById("newTaskForm").addEventListener("submit", function(event) {
+  /* Prevent page to be re-loaded */
+  event.preventDefault();
+  /* Get new task information */
+  const newTaskDescription = document.getElementById("newTaskDescription").value;
+  const newTaskDueDate = document.getElementById("newTaskDueDate").value;
+  /* Check input information, if correct */
+  if (!newTaskDescription) {
+    alert("You must fill the 'Task description' field before submitting!");
+  } else if (!newTaskDueDate) {
+    alert("You must fill the 'Due date' field before submitting!");
+  } else {
+    addNewTaskToTasksList(newTaskDescription, newTaskDueDate);
+  }
+});
